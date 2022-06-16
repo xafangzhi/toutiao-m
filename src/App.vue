335afe26cc3d9cfd6c84239ca142dd0c4fc8294a
zhoufang <491593576@qq.com>
2022-06-16 10:50:53 +0800

    添加vant组件

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
-    <router-view />  //路由对象，绝对不能删
+    <div >
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    </div>
+    <router-view />
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+// 加载vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 注册使用vant
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
